f9155b2462b21b8f1411a2755973069a8df14cc9
EddieHooks36 <eddiehooks36@gmail.com>
2023-02-26 11:57:04 -0800

    110 class 2

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
-from flask import Flask, abort
+from flask import Flask, abort, request
 from data import me, mock_catelog
+from config import db
+from bson import ObjectId
 
 import json
+
 
 app = Flask(__name__) # similar to new task in JS/create a new instance
 
@@ -38,57 +41,81 @@ def address():
     return f'{address["street"]} #{address["number"]}, {address["city"]}, {address["zip"]}'
 
 
+def fix_id(obj):
+    obj["_id"] = str(obj["_id"])
+
 @app.get("/api/catelog")
 def the_catelog():
-    return json.dumps(mock_catelog)
+    cursor = db.product.find({})
+    results = []
+    for prod in cursor:
+        fix_id(prod)
+
+    return json.dumps(results)
+
+
+@app.post("/api/catelog")
+def save_product():
+    data = request.get_json()
+    db.product.insert_one(data)
+
+    print("-" * 25)
+    print(data)
+    fix_id(data)
+
+    return json.dumps(data)
 
 
 @app.get("/api/catelog/count")
 def product_count():
-    count = len(mock_catelog)
-    return json.dumps(count)
+    total = db.product.count_documents({})
+    return json.dumps(total)
 
 
 @app.get("/api/category/<cat>")
 def prods_by_category(cat):
+    cursor = db.product.find({"category" : cat})
     results = []
-    for prod in mock_catelog:
-        if prod["category"] == cat:
-            results.append(prod)
-
+    for prod in cursor:
+        fix_id(prod)
+        results.append(prod)
 
     return json.dumps(results)
 
 
 @app.get("/api/product/<id>")
 def prod_by_id(id):
-    for prod in mock_catelog:
-        if prod["_id"] == id:
-          return json.dumps(id)
-
-
-    return abort(404) # Not Found
+    _id = ObjectId(id)
+    prod = db.product.find_one({"_id" : _id})
+    if prod is None:
+        return abort(404) # Not Found
+    
+    fix_id(prod)
+    return json.dumps(prod)
 
 
 @app.get("/api/product/search/<text>")
 def prod_title(text):
+      cursor = db.product.find({"title" : {"$regex" : text, "$options" : "i"}})
       results = []
-      for prod in mock_catelog:
-        if text.lower() in prod["title"].lower:
-            results.append(prod)
+      for prod in cursor:
+          results.append(prod)
+    #   for prod in mock_catelog:
+    #     if text.lower() in prod["title"].lower:
+    #         results.append(prod)
 
       return json.dumps(results)
 
 
 @app.get("/api/categories")
 def get_categories():
-    results = []
-    for prod in mock_catelog:
-        cat  = prod["category"]
-        if cat not in results:
-            results.append(cat)
+    cursor = db.product.distinct("category")
+    # for prod in mock_catelog:
+    #     cat  = prod["category"]
+    #     if cat not in results:
+    #         results.append(cat)
 
-    return json.dumps(results)
+    return json.dumps(list(cursor))
 
 
 
